010d0d1c762cb4ac45b003d50a145bc192aa602e
wama2308 <wmedina@master.inmobilia.com>
2023-05-30 18:39:24 -0400

    curso

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -5,24 +5,27 @@ import Head from 'next/head';
 import { Navbar } from '../Navbar';
 import styles from './MainLayout.module.css';
 
+interface Props {
+  children?: React.ReactNode | undefined
+}
 
-export const MainLayout: FC = ({ children }) => {
-    return (
-        <div className={styles.container}>
-          
-          <Head>
-            <title>Home - Fernando</title>
-            <meta name="description" content="Home Page" />
-            <link rel="icon" href="/favicon.ico" />
-          </Head>
-        
-          <Navbar />
-    
-          <main className={styles.main}>
-    
-            { children }
-    
-          </main>     
-        </div>
-      )
+export const MainLayout: FC<Props> = ({ children }) => {
+  return (
+    <div className={styles.container}>
+
+      <Head>
+        <title>Home - Fernando</title>
+        <meta name="description" content="Home Page" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+      <main className={styles.main}>
+
+        {children}
+
+      </main>
+    </div>
+  )
 };
